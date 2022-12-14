e9aa30c31a10ee7ec0998e94f3679e96d9301adf
Colin Campbell <colin.campbell321123@hotmail.com>
2022-12-14 17:51:41 +0000

    Part 2

--- bin/day_11.pl
+++ bin/day_11.pl
@@ -10,7 +10,7 @@ use lib "$FindBin::Bin/../lib";
 
 use MonkeyBusiness::Monkey;
 
-my @monkeys = (
+my @starting_monkeys = (
     MonkeyBusiness::Monkey->new(
         items            => [ 75, 63 ],
         operator         => '*',
@@ -77,7 +77,9 @@ my @monkeys = (
     )
 );
 
-# my @monkeys = (
+my $mod = 11 * 2 * 5 * 7 * 17 * 19 * 3 * 13;
+
+# my @starting_monkeys = (
 #     MonkeyBusiness::Monkey->new(
 #         items            => [ 79, 98 ],
 #         operator         => '*',
@@ -112,14 +114,166 @@ my @monkeys = (
 #     )
 # );
 
+# my $mod = 23 * 19 * 13 * 17;
+
+my @monkeys = (@starting_monkeys);
+
 for ( my $r = 0; $r < 20; $r++ ) {
+    my $monkeys_ref = run_round( \@monkeys, $mod, 0 );
+
+    @monkeys = @{$monkeys_ref};
+}
+
+my @inspection_counts;
+foreach my $monkey (@monkeys) {
+    push( @inspection_counts, $monkey->inspection_count );
+}
+
+@inspection_counts = sort { $b <=> $a } @inspection_counts;
+
+my $monkey_business_level = $inspection_counts[0] * $inspection_counts[1];
+
+print 'Part 1 answer: ' . $monkey_business_level . "\n";
+
+@starting_monkeys = (
+    MonkeyBusiness::Monkey->new(
+        items            => [ 75, 63 ],
+        operator         => '*',
+        operation_value  => '3',
+        test_value       => 11,
+        test_pass_monkey => 7,
+        test_fail_monkey => 2,
+    ),
+    MonkeyBusiness::Monkey->new(
+        items            => [ 65, 79, 98, 77, 56, 54, 83, 94 ],
+        operator         => '+',
+        operation_value  => '3',
+        test_value       => 2,
+        test_pass_monkey => 2,
+        test_fail_monkey => 0,
+    ),
+    MonkeyBusiness::Monkey->new(
+        items            => [66],
+        operator         => '+',
+        operation_value  => '5',
+        test_value       => 5,
+        test_pass_monkey => 7,
+        test_fail_monkey => 5,
+    ),
+    MonkeyBusiness::Monkey->new(
+        items            => [ 51, 89, 90 ],
+        operator         => '*',
+        operation_value  => '19',
+        test_value       => 7,
+        test_pass_monkey => 6,
+        test_fail_monkey => 4,
+    ),
+    MonkeyBusiness::Monkey->new(
+        items            => [ 75, 94, 66, 90, 77, 82, 61 ],
+        operator         => '+',
+        operation_value  => '1',
+        test_value       => 17,
+        test_pass_monkey => 6,
+        test_fail_monkey => 1,
+    ),
+    MonkeyBusiness::Monkey->new(
+        items            => [ 53, 76, 59, 92, 95 ],
+        operator         => '+',
+        operation_value  => '2',
+        test_value       => 19,
+        test_pass_monkey => 4,
+        test_fail_monkey => 3,
+    ),
+    MonkeyBusiness::Monkey->new(
+        items            => [ 81, 61, 75, 89, 70, 92 ],
+        operator         => '*',
+        operation_value  => 'old',
+        test_value       => 3,
+        test_pass_monkey => 0,
+        test_fail_monkey => 1,
+    ),
+    MonkeyBusiness::Monkey->new(
+        items            => [ 81, 86, 62, 87 ],
+        operator         => '+',
+        operation_value  => '8',
+        test_value       => 13,
+        test_pass_monkey => 3,
+        test_fail_monkey => 5,
+    )
+);
+
+# my @starting_monkeys = (
+#     MonkeyBusiness::Monkey->new(
+#         items            => [ 79, 98 ],
+#         operator         => '*',
+#         operation_value  => '19',
+#         test_value       => 23,
+#         test_pass_monkey => 2,
+#         test_fail_monkey => 3,
+#     ),
+#     MonkeyBusiness::Monkey->new(
+#         items            => [ 54, 65, 75, 74 ],
+#         operator         => '+',
+#         operation_value  => '6',
+#         test_value       => 19,
+#         test_pass_monkey => 2,
+#         test_fail_monkey => 0,
+#     ),
+#     MonkeyBusiness::Monkey->new(
+#         items            => [ 79, 60, 97 ],
+#         operator         => '*',
+#         operation_value  => 'old',
+#         test_value       => 13,
+#         test_pass_monkey => 1,
+#         test_fail_monkey => 3,
+#     ),
+#     MonkeyBusiness::Monkey->new(
+#         items            => [74],
+#         operator         => '+',
+#         operation_value  => '3',
+#         test_value       => 17,
+#         test_pass_monkey => 0,
+#         test_fail_monkey => 1,
+#     )
+# );
+
+@monkeys = (@starting_monkeys);
+
+for ( my $r = 0; $r < 10000; $r++ ) {
+    my $monkeys_ref = run_round( \@monkeys, $mod, 1 );
+
+    @monkeys = @{$monkeys_ref};
+}
+
+@inspection_counts = ();
+foreach my $monkey (@monkeys) {
+    push( @inspection_counts, $monkey->inspection_count );
+}
+
+@inspection_counts = sort { $b <=> $a } @inspection_counts;
+
+$monkey_business_level = $inspection_counts[0] * $inspection_counts[1];
+
+print 'Part 2 answer: ' . $monkey_business_level . "\n";
+
+sub run_round {
+    my $monkeys_ref = shift;
+    my $mod         = shift;
+    my $part2       = shift;
+
+    my @monkeys = @{$monkeys_ref};
+
     foreach my $monkey (@monkeys) {
         my @items = @{ $monkey->items };
 
         for ( my $i = 0; $i < scalar(@items); $i++ ) {
             my $new_worry_level = $monkey->operation( $items[$i] );
 
-            $new_worry_level = int( $new_worry_level / 3 );
+            $new_worry_level = $new_worry_level % $mod;
+
+            unless ($part2) {
+                $new_worry_level = int( $new_worry_level / 3 );
+            }
 
             if ( $new_worry_level % $monkey->test_value == 0 ) {
                 my @other_monkey_items
@@ -146,15 +300,6 @@ for ( my $r = 0; $r < 20; $r++ ) {
         @items = ();
         $monkey->items( \@items );
     }
+
+    return \@monkeys;
 }
-
-my @inspection_counts;
-foreach my $monkey (@monkeys) {
-    push( @inspection_counts, $monkey->inspection_count );
-}
-
-@inspection_counts = sort { $b <=> $a } @inspection_counts;
-
-my $monkey_business_level = $inspection_counts[0] * $inspection_counts[1];
-
-print 'Part 1 answer: ' . $monkey_business_level . "\n";
--- t/integration_tests/day_11.t
+++ t/integration_tests/day_11.t
@@ -7,6 +7,9 @@ use Test2::V0;
 
 my $output_string = `perl bin/day_11.pl`;
 
-is( $output_string, "Part 1 answer: 62491\n", 'Day 11 output is correct' );
+is( $output_string,
+    "Part 1 answer: 62491\nPart 2 answer: 17408399184\n",
+    'Day 11 output is correct'
+);
 
 done_testing();
